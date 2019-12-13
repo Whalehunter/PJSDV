850b271a6ccec8073f8105183d45c9d7c3267155
Bart14074591 <58037705+Bart14074591@users.noreply.github.com>
2019-12-13 14:14:07 +0100

    Update Tafellamp.ino

--- Arduino Code/Tafellamp/Tafellamp/Tafellamp.ino
+++ Arduino Code/Tafellamp/Tafellamp/Tafellamp.ino
@@ -1,9 +1,15 @@
 #include <Wire.h>
+#include <Adafruit_NeoPixel.h>
 
 #define I2C_SDL   D1
 #define I2C_SDA   D2
+#define RGB       D5
+#define NUMPIXELS 1
+
+Adafruit_NeoPixel pixels(NUMPIXELS, RGB, NEO_GRB + NEO_KHZ800);
 
 int leesPIR(); 
+void RGBstrip();
 
 void setup() {
   Wire.begin();//Start wire
@@ -23,4 +29,14 @@ int leesPIR(){
   Wire.requestFrom(0x38, 1);          
   int reading = Wire.read();
   return reading;
+}
+
+void RGBstrip(){
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.show();  // Send the updated pixel colors to the hardware
+  delay(500);
+  for(int i=0; i<NUMPIXELS; i++) { //Elke pixel aanzetten
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0)); // Set the pixel to green
+    pixels.show();   // Send the updated pixel colors to the hardware
+  }
 }
