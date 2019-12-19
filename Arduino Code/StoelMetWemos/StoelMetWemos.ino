219755d4231e8d5fca390155533b679d4d8708f5
LordMittens <57997780+LordMittens@users.noreply.github.com>
2019-12-19 13:33:29 +0100

    Update StoelMetWemos.ino

--- Arduino Code/StoelMetWemos/StoelMetWemos.ino
+++ Arduino Code/StoelMetWemos/StoelMetWemos.ino
@@ -4,6 +4,10 @@
 #define I2C_SDL    D1
 #define I2C_SDA    D2
 
+void lampAanUit(int i);
+void trilSensorAanUit(int i);
+
+int hex = 0x00;
 int port = 8883;
 WiFiServer server(port);
 const char* host = "192.168.4.1";
@@ -17,7 +21,7 @@ void setup(void) {
   
   Wire.begin();  //Start wire.
   Serial.begin(115200);  //Set serial Baud.
-
+/*
   WiFi.mode(WIFI_STA);
   WiFi.hostname("wemos");
   WiFi.begin(ssid, password);
@@ -48,8 +52,10 @@ void setup(void) {
 
     Serial.println("[Sending a request]");
     client.print(String("HALLO"));
-  }
+  }*/
 }
+
+
 
 void loop(void) {
   WiFiClient client = server.available();
@@ -57,56 +63,98 @@ void loop(void) {
   digitalWrite(D5, HIGH);  //LED D4 ON.
   delay(500); 
   
+  lampAanUit(1);
+  trilSensorAanUit(0);
+  
+  String line = "";
+
+  
+  if (client.connect(host, port)){
+    line = client.readStringUntil('\r');
+    if (line == "ID?"){
+      client.print(String("ei"));
+    }
+    line = client.readStringUntil('\r');
+    if (line == "OK"){  
+      while (client.connected() || client.available()){
+        Serial.println("Connected!");
+        Wire.beginTransmission(0x38); 
+        Wire.write(byte(0x00));      
+        Wire.endTransmission();
+        Wire.requestFrom(0x38, 1);  //request data from 0x38   
+        unsigned int inputs = Wire.read();//Write value on address of inputs.
+        Serial.print("Digital in: "); 
+        Serial.println(inputs&0x01);
+        
+        Wire.requestFrom(0x36, 4);  //request data from 0x36   
+        unsigned int anin0 = Wire.read()&0x03;  
+        anin0=anin0<<8;
+        anin0 = anin0|Wire.read();
+        Serial.println(anin0);
+  
+        if (anin0 >= 100){
+          client.print(String("STOEL: Druksensor") + anin0);
+        }
+        if ((inputs&0x01) == 1){
+          //client.print(String("STOEL: Knoppie") + (inputs&0x01));
+          client.print(String("a"));
+          String line = client.readStringUntil('\r');
+            if (line == "b"){
+              Serial.println("kaas");
+              line = "a";
+            }
+          Serial.println(line);
+        }
+        delay(1000);
+      }
+      client.stop();
+    }
+  }
+  digitalWrite(D5, LOW);  //LED D4 OFF.
+  delay(500);
+}
+
+void trilSensorAanUit(int i){
   Wire.beginTransmission(0x38);
   Wire.write(byte(0x03));           
   Wire.write(byte(0x0F));            
   Wire.endTransmission();
 
-  Wire.beginTransmission(0x38);
-  Wire.write(byte(0x01));           
-  Wire.write(byte(0x00));            
-  Wire.endTransmission();
-  
-  if (client.connect(host, port)){
-    while (client.connected() || client.available()){
-      
-      Wire.beginTransmission(0x38); 
-      Wire.write(byte(0x00));      
-      Wire.endTransmission();
-      Wire.requestFrom(0x38, 1);  //request data from 0x38   
-      unsigned int inputs = Wire.read();//Write value on address of inputs.
-      Serial.print("Digital in: "); 
-      Serial.println(inputs&0x01);
-      
-      Wire.requestFrom(0x36, 4);  //request data from 0x36   
-      unsigned int anin0 = Wire.read()&0x03;  
-      anin0=anin0<<8;
-      anin0 = anin0|Wire.read();
-      Serial.println(anin0);
-
-      if (anin0 >= 100){
-        client.print(String("STOEL: Druksensor") + anin0);
-      }
-      if ((inputs&0x01) == 1){
-        //client.print(String("STOEL: Knoppie") + (inputs&0x01));
-        client.print(String("a"));
-        String line = client.readStringUntil('\r');
-          if (line == "b"){
-            Serial.println("kaas");
-            line = "a";
-          }
-        Serial.println(line);
-      }
-      delay(1000);
-    }
-    client.stop();
+  if (i == 1){
+    Wire.beginTransmission(0x38);
+    Wire.write(byte(0x01));           
+    Wire.write(byte(hex |= 0x20));            
+    Wire.endTransmission();
   }
-  
-  digitalWrite(D5, LOW);  //LED D4 OFF.
-  delay(500);
+  else {
+    Wire.beginTransmission(0x38);
+    Wire.write(byte(0x01));           
+    Wire.write(byte(hex &= !(0x20)));            
+    Wire.endTransmission(); 
+  }
+  //delay(1000);
 }
 
+void lampAanUit(int i){
+  Wire.beginTransmission(0x38);
+  Wire.write(byte(0x03));           
+  Wire.write(byte(0x0F));            
+  Wire.endTransmission();
 
+  if (i == 1){
+    Wire.beginTransmission(0x38);
+    Wire.write(byte(0x01));           
+    Wire.write(byte(hex |= 0x10));            
+    Wire.endTransmission();
+  }
+  else {
+    Wire.beginTransmission(0x38);
+    Wire.write(byte(0x01));           
+    Wire.write(byte(hex &= !(0x10)));            
+    Wire.endTransmission(); 
+  }
+  //delay(1000);
+}
 
 
 
