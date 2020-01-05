3a62886a764c58090c91b9ed17781b3a7c71f95e
Bart14074591 <58037705+Bart14074591@users.noreply.github.com>
2020-01-05 13:03:02 +0100

    Update Bed_test.ino

--- Arduino Code/Bed/Bed_test/Bed_test.ino
+++ Arduino Code/Bed/Bed_test/Bed_test.ino
@@ -59,13 +59,15 @@ void loop() {
           int knopwaarde = leesKnop();
           //client.print(String(drukknopwaarde));
           client.print(String(knopwaarde));          
-          line = '0';
+          line = "0";
         }
         else if (line == "lampAan"){
           LedAanUit(1);
+          line = "0";
         }
         else if (line == "lampUit"){
           LedAanUit(0);
+          line = "0";
         }
       }
     }
