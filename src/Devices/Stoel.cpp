6c9d7e912c5fd364b57ec4c9dd29861f70d17daa
jeff <j.hoogh91@gmail.com>
2020-01-08 16:11:32 +0100

    hoi

--- src/Devices/Stoel.cpp
+++ src/Devices/Stoel.cpp
@@ -17,46 +17,50 @@ Stoel::~Stoel()
 
 void Stoel::operator()()//Overloaded functies moeten met 2 haakjes zodat je er zoveel mogelijk over kan sturen als je wilt.
 {
-	char buffer[256];
+    char buffer[256];
 
-	while(1) {
-		/* get and store JSON values */
+    while(1) {
+        /* get and store JSON values */
 
-		memset(buffer, 0, sizeof(buffer));
-		strcpy(buffer, "getStatus\r");
-		sendMsg(buffer);
+        memset(buffer, 0, sizeof(buffer));
+        strcpy(buffer, "getStatus\r");
+        sendMsg(buffer);
 
-		memset(buffer, 0, sizeof(buffer));
-		if(recv(sock, buffer, 255, 0) < 1) { // dit wordt een functie
-			std::cout << "Deur disconnected from socket: " << sock << std::endl;
-			close(sock);
-			return;
-		}
+        memset(buffer, 0, sizeof(buffer));
+        if(recv(sock, buffer, 255, 0) < 1) { // dit wordt een functie
+            std::cout << "Deur disconnected from socket: " << sock << std::endl;
+            close(sock);
+            return;
+        }
 
-		auto j_deur = json::parse(buffer); // hier moeten ook exceptions afgehandeld worden
+        try {
+            auto j_deur = json::parse(buffer); // hier moeten ook exceptions afgehandeld worden
 
-		drukknop = j_deur.at("drukknop");
-		trilStatus = j_deur.at("trilStatus");
+            drukknop = j_deur.at("drukknop");
+            trilStatus = j_deur.at("trilStatus");
+        }
+        catch(json::parse_error) {
+            std::cout << "parse error" << std::endl;
+        }
+        if ((drukknop == 1) & (ledStatus == 0)){
+            sendMsg("ledAan/r");
+            ledStatus = 1;
+        }
+        else if ((drukknop == 1) & (ledStatus == 1)){
+            sendMsg("ledUit/r");
+            ledStatus = 0;
+        }
 
-		if ((drukknop == 1) & (ledStatus == 0)){
-				sendMsg("ledAan/r");
-				ledStatus = 1;
-		}
-		else if ((drukknop == 1) & (ledStatus == 1)){
-			sendMsg("ledUit/r");
-			ledStatus = 0;
-		}
-
-		if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 0)){
-				sendMsg("trilAan/r");
-				trilStatus = 1;
-			}
-		else if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 1)){
-			sendMsg("trilUit/r");
-			trilStatus = 0;
-		}
-		/*ledAanUit();
-		trilAanUit();*/
+        if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 0)){
+            sendMsg("trilAan/r");
+            trilStatus = 1;
+        }
+        else if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 1)){
+            sendMsg("trilUit/r");
+            trilStatus = 0;
+        }
+        /*ledAanUit();
+          trilAanUit();*/
     }
     close(sock);
     std::cout << "Connection closed on socket " << sock << std::endl;
@@ -68,24 +72,24 @@ json Stoel::getStatus()
 }
 
 void Stoel::ledAanUit(){
-	if ((drukknop == 1) & (ledStatus == 0)){
-		sendMsg("ledAan/r");
-		ledStatus = 1;
-	}
-	else if ((drukknop == 1) & (ledStatus == 1)){
-		sendMsg("ledUit/r");
-		ledStatus = 0;
-	}
+    if ((drukknop == 1) & (ledStatus == 0)){
+        sendMsg("ledAan/r");
+        ledStatus = 1;
+    }
+    else if ((drukknop == 1) & (ledStatus == 1)){
+        sendMsg("ledUit/r");
+        ledStatus = 0;
+    }
 }
 
 void Stoel::trilAanUit(){
-	if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 0)){
-		sendMsg("trilAan/r");
-		trilStatus = 1;
-	}
-	else if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 1)){
-		sendMsg("trilUit/r");
-		trilStatus = 0;
-	}
+    if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 0)){
+        sendMsg("trilAan/r");
+        trilStatus = 1;
+    }
+    else if ((drukknop == 1) & (druksensor == 1) & (trilStatus == 1)){
+        sendMsg("trilUit/r");
+        trilStatus = 0;
+    }
 }
 
