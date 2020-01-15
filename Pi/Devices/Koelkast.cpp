5fe495be9851a1904413d72b19ed88ed43637d96
LordMittens <57997780+LordMittens@users.noreply.github.com>
2020-01-15 17:42:04 +0100

    Add files via upload

--- Pi/Devices/Koelkast.cpp
+++ Pi/Devices/Koelkast.cpp
@@ -9,7 +9,7 @@
 
 using json = nlohmann::json;
 
-Koelkast::Koelkast(int n, Appartement* ap): Device(n, ap){
+Koelkast::Koelkast(int n, Appartement* ap): Device(n, ap), openTimer(0){
     std::cout << "Koelkast aangemaakt" << std::endl;
 }
 
@@ -44,22 +44,25 @@ void Koelkast::operator()(){
 			std::cout << "parse error" << std::endl;
 		}
 
-		if ((koelkastDeur == 0) && !(((std::clock() - timer) / (double) CLOCKS_PER_SEC) >= 5.0)){
-			peltierUit();
-			if (((std::clock() - timer) / (double) CLOCKS_PER_SEC) == 0.0){
-				timer = std::clock();
-			}
+		if ((koelkastDeur == 0) && (((std::clock() - openTimer) / (double) CLOCKS_PER_SEC) <= 5.0)){
+			peltierUit();/*
+			if (((std::clock() - openTimer) / (double) CLOCKS_PER_SEC) == 0.0){
+				openTimer = std::clock();
+
+			}*/
 		}
-		else if ((koelkastDeur == 0) && (((std::clock() - timer) / (double) CLOCKS_PER_SEC) >= 5.0)){
+		else if ((koelkastDeur == 0) && (((std::clock() - openTimer) / (double) CLOCKS_PER_SEC) >= 5.0)){
 			koelAlarm = 1;
 			fanUit();
 			peltierUit();
 		}
 		else{
+			openTimer = std::clock();
 			peltierAan();
 			fanAan();
-			timer = 0;
+
 		}
+		std::cout << openTimer << std::endl;
 	}
 	close(sock);
 	std::cout << "Connection closed on socket " << sock << std::endl;
