169f2aca9f97d8c892093322b23cd5c7f8f919af
Jeffrey Meissen <whale@cock.lu>
2020-01-20 11:49:37 +0100

    Discostand toegevoegd

--- GUIs/Bewaker/index.php
+++ GUIs/Bewaker/index.php
@@ -268,7 +268,7 @@
           <h2 class="header">Muur</h2>
           <div class="ui divider"></div>
           <div class="description">
-            <div class="ui divided three column grid">
+            <div class="ui divided two column grid">
               <div class="column lamp">
                 <p><b>Lamp</b></p>
                 <div class="ui fitted toggle checkbox">
@@ -287,6 +287,12 @@
                 <div class="ui fitted toggle checkbox">
                     <input name="" type="checkbox" value=""/>
                 </div>
+              </div>
+              <div class="column disco">
+                  <p><b>Discostand</b></p>
+                  <div class="ui fitted slider checkbox">
+                      <input name="" type="checkbox" value=""/>
+                  </div>
               </div>
             </div>
           </div>
